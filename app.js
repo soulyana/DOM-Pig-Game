5bba257a3d2a6300ff3026ce0c2a3c383b1be7e7
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-29 20:21:39 -0400

    Events and event handling: rolling the dice

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ activePlayer = 1;
 //to hide dice upon loading project
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Random number

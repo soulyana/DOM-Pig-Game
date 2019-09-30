ca6ac8b53597d476c437202911c16b2f5b5daad5
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-29 20:18:42 -0400

    Events and event handling: Rolling the dice

--- app.js
+++ app.js
@@ -9,19 +9,34 @@ GAME RULES:
 
 */
 
-var dice, scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1; 
 
-dice = Math.floor(Math.random() * 6) + 1;
-// Selects dynamically current player
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 //to hide dice upon loading project
 document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+})
+
+
+
+
+// Selects dynamically current player
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);

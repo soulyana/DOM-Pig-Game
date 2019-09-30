af2061af16c225e9e6e0b56e153f1c43803668ce
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-29 20:38:50 -0400

    updating scores and changing the active player

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 1; 
+activePlayer = 0;
 
 //to hide dice upon loading project
 document.querySelector('.dice').style.display = 'none';
@@ -24,7 +24,7 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
+document.querySelector('.btn-roll').addEventListener('click', function () {
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
 
@@ -34,13 +34,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        // Add score 
+        roundScore += dice;
+        // Selects dynamically current player
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
 })
 
 
 
 
-// Selects dynamically current player
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 // var x = document.querySelector('#score-0').textContent;
